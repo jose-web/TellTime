6aae7bcff1de84dcc7b0b239ea89eaad1d818e5b
jose <delossantosramosjose@gmail.com>
2021-05-08 22:47:08 +0200

    Los botones y el input personalizado funcionan

--- background.js
+++ background.js
@@ -1,28 +1,41 @@
+let TIMEOUT
+let INTERVAL
 function load() {
-    let minutos = 1
-    let timeout = cuentaMinutos(minutos)
-
-    setTimeout(function () {
-        speak()
-        let interval = cuentaMinutos(minutos)
-        setInterval(function () {
-            speak()
-        }, interval)
-    }, timeout)
+    crear(30)
 
     chrome.extension.onConnect.addListener(function (port) {
         port.onMessage.addListener(function (msg) {
             switch (msg) {
-                case "":
+                case "hora":
+                    crear(60)
+                    break
 
+                case "mediaHora":
+                    crear(30)
                     break
 
                 default:
-                    speak()
+                    crear(msg)
                     break
             }
         })
     })
+}
+
+function crear(minutos) {
+    console.log(minutos);
+    clearTimeout(TIMEOUT)
+    clearInterval(INTERVAL)
+
+    let miliTimeout = cuentaMinutos(minutos)
+
+    TIMEOUT = setTimeout(function () {
+        speak()
+        let miliInterval = cuentaMinutos(minutos)
+        INTERVAL = setInterval(function () {
+            speak()
+        }, miliInterval)
+    }, miliTimeout)
 }
 
 function cuentaMinutos(minutos) {
---
+++ popup/popup.js
@@ -0,0 +1,19 @@
+function load() {
+    var port = chrome.extension.connect({
+        name: "tellTime"
+    });
+
+    var opcion = document.getElementsByName("opcion");
+    for (var i = 0; i < opcion.length; i++) {
+        opcion[i].addEventListener('click', function () {
+            if (this.value != "personalizado")
+                port.postMessage(this.value)
+        })
+    }
+
+    document.getElementById("numero").addEventListener('focusout', function () {
+        port.postMessage(Number(this.value))
+    })
+}
+
+window.onload = load
