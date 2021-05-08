8f27652f1d1f1e6f99b39e3ea3b01892cd6ced93
jose <delossantosramosjose@gmail.com>
2021-05-08 19:05:05 +0200

    añadida la capacidad para calcular y decir la hora cada un minuto

--- background.js
+++ background.js
@@ -1,7 +1,34 @@
 function load() {
-    const text = 'son las 8'
-    const synth = window.speechSynthesis
-    const utterThis = new SpeechSynthesisUtterance(text)
-    synth.speak(utterThis)
+    let minutos = 1
+    let timeout = cuentaMinutos(minutos)
+
+    setTimeout(function () {
+        speak()
+        let interval = cuentaMinutos(minutos)
+        setInterval(function () {
+            speak()
+        }, interval)
+    }, timeout);
 }
-window.onload = load;
+
+function cuentaMinutos(minutos) {
+    let d = new Date()
+    let d2 = new Date()
+
+    d2.setMinutes(d2.getMinutes() + minutos)
+    d2.setSeconds(0)
+    d2.setMilliseconds(500)
+
+    let total = d2.getTime() - d.getTime()
+
+    return total
+}
+
+function speak() {
+    let d = new Date()
+    let hora = " son las " + (d.getHours() > 12 ? d.getHours() - 12 : d.getHours()) + " y " + d.getMinutes()
+    let utterThis = new SpeechSynthesisUtterance(hora)
+    window.speechSynthesis.speak(utterThis)
+}
+
+window.onload = load
